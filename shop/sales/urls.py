480deeaccc9f8d8717c4aa1e348417e8fa006d30
sagar <sagarsandha550@gmail.com>
2024-11-13 15:26:40 +0545

    push

--- shop/sales/urls.py
+++ shop/sales/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     
     path('', views.sales_entry, name='sales_entry'),
-    
+    path("daily_expense/", views.daily_expense, name='daily_expense'),
 ]
--- shop/sales/views.py
+++ shop/sales/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def sales_entry(request):
     return render(request, 'sales/sales_entry.html')
+def daily_expense(request):
+    return render(request, 'sales/daily_expense.html')
